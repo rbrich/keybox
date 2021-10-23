3df7928f5b8d828065be0a535bf7ec350cbfc91d
Radek Brich <radek.brich@devl.cz>
2021-10-23 22:59:51 +0200

    setup.py - add long_description for PYPI

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from pathlib import Path
+setup_dir = Path(__file__).parent
 
 setup(
     name='keybox',
-    version=open('VERSION', 'r').read().strip(),
+    version=(setup_dir / 'VERSION').read_text().strip(),
     description='Storage for passwords, encrypted with GPG',
+    long_description=(setup_dir / "README.rst").read_text(),
+    long_description_content_type='text/x-rst',
     author='Radek Brich',
     author_email='radek.brich@devl.cz',
     license='MIT',
