74006cd066ff2d4ba0dce3c84de5058242105090
Radek Brich <radek.brich@devl.cz>
2021-10-28 14:21:10 +0200

    envelope format - document extension points

--- keybox/envelope.py
+++ keybox/envelope.py
@@ -5,12 +5,13 @@ import nacl.pwhash
 import nacl.utils
 import nacl.secret
 import base64
-from io import BytesIO
 import struct
+from io import BytesIO
 
-# Header format (integers are little endian):
+# Header format
+# =============
 # 4 bytes MAGIC - zero-terminated string "[K]"
-# 4 bytes META_SIZE - total size of the following metadata chunks
+# 4 bytes META_SIZE (u32) - total size of the following metadata chunks
 # META_SIZE bytes META_DATA - chunks, see below
 #
 # Each chunk:
@@ -19,19 +20,40 @@ import struct
 # SIZE bytes VALUE
 #
 # Supported tags:
-# 0 - reserved (could be version)
-# 1 - DATA_SIZE - size of encrypted data that follow the header - 32bit unsigned integer
-# 2 - CIPHER - symmetric encryption algorithm, 8bit unsigned integer:
+# 0 - END - terminates the chunk reading, allows extra data in the header
+#           (the chunk SIZE must follow, making the terminator a sequence of two zero bytes)
+# 1 - DATA_SIZE (u32) - size of encrypted data that follows the header
+# 2 - CIPHER (u8) - symmetric encryption algorithm:
 #              1 = XSalsa20 + Poly1305 MAC (default)
-# 3 - PWHASH - key derivation function, 8bit unsigned integer:
+# 3 - PWHASH (u8) - key derivation function:
 #              1 = scrypt
 #              2 = argon2i (default)
 #              3 = argon2id
-# 4 - SALT - string
-# 5 - OPS_LIMIT - 8bit unsigned integer
-# 6 - MEM_LIMIT - 32bit unsigned integer
+# 4 - SALT (str)
+# 5 - OPS_LIMIT (u8)
+# 6 - MEM_LIMIT (u32)
+#
+# Types:
+# (u8, u16, u32, u64) unsigned integers, little endian
+#       - the size in the above specification is the default for writing
+#       - when reading, the integer size is controlled by the chunk SIZE
+# (str) string of bytes / binary data - the length is limited by 1 byte SIZE, i.e. 255
+#
+# Extension points:
+# - META_DATA area can be bigger than the contained chunks (last chunk can be '\0\0' - a terminator)
+#       - anything after the marker is currently ignored
+# - 8 + META_SIZE + DATA_SIZE can be smaller than the actual file
+#       - anything after the encrypted data and before EOF is currently ignored
+# - every integer field supports all sizes (8bit to 64bit)
+#       - DATA_SIZE can be 64bit, only META_SIZE is truly limited to 32bit
+#       - CIPHER, PWHASH enums can extend to another byte if needed
+# - no versioning, the program tries to read the file and ignores unknown chunks (emits a warning)
+#       - this allows adding new optional chunks without breaking the ability of old versions
+#         of the program to read it (if it were driven by a version, the old program would be
+#         obliged to reject the file)
 
 MAGIC = b'[K]\0'  # nul byte marks the file as binary without depending on the content itself
+TAG_END = 0
 TAG_DATA_SIZE = 1
 TAG_CIPHER = 2
 TAG_PWHASH = 3
@@ -98,26 +120,36 @@ class Envelope:
         meta_data = f.read(meta_size)
         data_size = -1
         chunks = BytesIO(meta_data)
+
+        def unpack_uint(v):
+            if len(v) == 1: return struct.unpack('<B', v)[0]
+            if len(v) == 2: return struct.unpack('<H', v)[0]
+            if len(v) == 4: return struct.unpack('<L', v)[0]
+            if len(v) == 8: return struct.unpack('<Q', v)[0]
+            raise Exception("Corrupted file envelope (not uint: %r)" % v)
+
         while True:
             # read a chunk
             raw_tag_size = chunks.read(2)
             if len(raw_tag_size) == 0:
-                break  # no more chunks
+                break  # implicit end of chunks - no more data
             tag, size = struct.unpack('BB', raw_tag_size)
+            if tag == TAG_END:
+                break  # explicit end of chunks
             value = chunks.read(size)
             if tag == TAG_DATA_SIZE:
-                data_size = struct.unpack('<I', value)[0]
+                data_size = unpack_uint(value)
             elif tag == TAG_CIPHER:
-                assert struct.unpack('<B', value)[0] == CIPHER_XSALSA20_POLY1305
+                assert unpack_uint(value) == CIPHER_XSALSA20_POLY1305
             elif tag == TAG_PWHASH:
-                self._pwhash_algo = struct.unpack('<B', value)[0]
+                self._pwhash_algo = unpack_uint(value)
                 self._pwhash = PWHASH_MODULE[self._pwhash_algo]
             elif tag == TAG_SALT:
                 self._salt = value
             elif tag == TAG_OPS_LIMIT:
-                self._ops_limit = struct.unpack('<B', value)[0]
+                self._ops_limit = unpack_uint(value)
             elif tag == TAG_MEM_LIMIT:
-                self._mem_limit = struct.unpack('<I', value)[0]
+                self._mem_limit = unpack_uint(value)
             else:
                 print(f"WARNING: File contains unknown chunk with tag {tag}, size {size}. "
                       "It might be created by future version of keybox program. Please update...")
